6ce5598b6c4bdb8aab6c5dc13b8291b1e3177a0d
PKH0208 <137423200+PKH0208@users.noreply.github.com>
2023-07-24 15:36:35 +0900

    Create Ani.java(최종본) - 박경호
    main - java - animal - Ani.java

---
+++ Ani.java
@@ -0,0 +1,69 @@
+package animal;
+
+public class Ani {
+	   private String ser;
+	   private String email;
+	   private String groupid;
+	   private String neuter;
+	   private String name;
+	   private String age;
+	   private String gender;
+	   private String picture;
+	   
+	   
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSer() {
+		return ser;
+	}
+	public void setSer(String ser) {
+		this.ser = ser;
+	}
+	public String getGroupid() {
+		return groupid;
+	}
+	public void setGroupid(String groupid) {
+		this.groupid = groupid;
+	}
+	public String getNeuter() {
+		return neuter;
+	}
+	public void setNeuter(String neuter) {
+		this.neuter = neuter;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	@Override
+	public String toString() {
+		return "Ani [ser=" + ser + ", email=" + email + ", groupid=" + groupid + ", neuter=" + neuter + ", name=" + name
+				+ ", age=" + age + ", gender=" + gender + ", picture=" + picture + "]";
+	}
+
+	   
+}
